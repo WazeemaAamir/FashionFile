4904e85ffc8c62455138cb94a44b79a856d488c1
WAZEEMA AAMIR <161026876+WazeemaAamir@users.noreply.github.com>
2025-06-02 23:56:30 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+
+import PracticeForm from "./Components/PracticeForm"
+import Contact from "./Contact/page";
+export default function Home() {
+  return (
+     <div>
+      <PracticeForm />
+      <Contact />
+    </div>
+  );
+}
